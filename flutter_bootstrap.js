c8a345d22b204a118e86ab378b12cf4b9feaf3a5
ivanobuonocore <ivanobuonocore@icloud.com>
2026-07-23 16:30:49 +0000

    Deploying to gh-pages from @ ivanobuonocore/flex.index.html@113021d62f66538d862faf0896f18330de2e58ab 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2263926752"
+    serviceWorkerVersion: "3833196770"
   }
 });
